d93c22454ce378e4e95257a62a1e5c8d85b287ac
VissentvNoord <49689172+VissentvNoord@users.noreply.github.com>
2023-10-27 12:20:43 +0200

    Tic tac toe fully works

--- scripts/game.js
+++ scripts/game.js
@@ -15,6 +15,9 @@ const GameBoard = (function(){
     const getCells = () => gameBoard;
 
     const trySetCellState = function(index){
+        if(!gameFlow.isGameStarted())
+            return;
+
         const newState = gameFlow.getCurrentPlayer().getSign();
 
         const cell = gameBoard[index];
@@ -35,8 +38,23 @@ const GameBoard = (function(){
         const isVerticalCor = checkVertical();
         console.log(isVerticalCor);
 
-        if(checkHorizontal() || checkVertical()){
+        if(checkHorizontal() || checkVertical() || checkDiagonal()){
             console.log("A player has won! using this sign: " + desiredState);
+
+            gameFlow.gameOver(false, desiredState);
+        }else if(boardFilled()){
+            gameFlow.gameOver(true, desiredState);
+        }
+
+        function boardFilled(){
+            let filled = true;
+            for(let i = 0; i < gameBoard.length; i++){
+                if(gameBoard[i].getState() == ""){
+                    filled = false;
+                }
+            }
+
+            return filled;
         }
 
         function checkVertical(){
@@ -96,10 +114,59 @@ const GameBoard = (function(){
         }
 
         function checkDiagonal(){
-            
+            let diagonalCorrelation = false;
+
+            let topCell;
+            let middleCell = gameBoard[4];
+            let bottomCell;
+
+            if(index < 3){
+                if(index != 1){
+                    topCell = gameBoard[index];
+
+                    if(index == 0){
+                        bottomCell = gameBoard[gameBoard.length - 1];
+                    }else if(index == 2){
+                        bottomCell = gameBoard[6];
+                    }
+                }
+            }else if(index < 6){
+                if(index == 4){
+                    //Is center so check all directions
+                    let topLeft = gameBoard[0];
+                    let bottomRight = gameBoard[8];
+
+                    let topRight = gameBoard[2];
+                    let bottomLeft = gameBoard[6];
+
+                    if(isCorrelation(topLeft, middleCell, bottomRight) || isCorrelation(topRight, middleCell, bottomLeft)){
+                        diagonalCorrelation = true;
+                    }
+                }
+            }else{
+                if(index != 7){
+                    bottomCell = gameBoard[index];
+
+                    if(index == 8){
+                        topCell = gameBoard[0];
+                    }else if(index == 6){
+                        topCell = gameBoard[2];
+                    }
+                }
+            }
+
+            if(isCorrelation(topCell, middleCell, bottomCell)){
+                diagonalCorrelation = true;
+            }
+
+            return diagonalCorrelation;
         }
 
         function isCorrelation(cell1, cell2, cell3){
+            if(cell1 == null || cell2 == null || cell3 == null){
+                return false;
+            }
+
             let cor = false;
             if(cell1.getState() == desiredState && cell2.getState() == desiredState && cell3.getState() == desiredState){
                  cor = true;
@@ -116,6 +183,7 @@ const GameBoard = (function(){
 const displayController = (function(){
     const board = document.getElementById("board");
     const playerboxes = document.getElementsByClassName("playerbox");
+    const winnerBox = document.getElementsByClassName("winnerbox")[0];
 
     const displayCell = function(cell){
         //Creating new cell and assigning the index to the html element
@@ -169,7 +237,20 @@ const displayController = (function(){
         newplayer.getUIElement().setAttribute(`data-current`, true);
     }
 
-    return {displayCell, setListeners, updateCell, displayPlayer, onCurrentPlayerSwitch};
+    const showEndPanel = function(player = null){
+        winnerBox.id = "visible";
+        const finishingText = winnerBox.children[0];
+        if(player == null){
+            //Its a draw
+            finishingText.innerHTML = "It's a draw!";
+        }else{
+            //A player has won
+
+            finishingText.innerHTML = `${player.getName()} has won!`;
+        }
+    }
+
+    return {displayCell, setListeners, updateCell, displayPlayer, onCurrentPlayerSwitch, showEndPanel};
 })();
 
 //Creating a cell that has needed data
@@ -206,11 +287,16 @@ function createPlayer(name, _sign){
 const gameFlow = (function(){
     const players = [];
     let currentPlayer;
+    let gameStarted;
+
+    const isGameStarted = () => gameStarted;
     
     const startGame = function(){
         GameBoard.createBoard();
         createPlayers();
         setCurrentPlayer(players[0]);
+
+        gameStarted = true;
     }
 
     //Initialize
@@ -240,17 +326,47 @@ const gameFlow = (function(){
         }
     }
 
+    const getPlayerFromState = function(state){
+        for(let i = 0; i < players.length; i++){
+            if(players[i].getSign() == state){
+                return players[i];
+            }
+        }
+
+        return null;
+    }
+
     const getCurrentPlayer = () => currentPlayer;
     const setCurrentPlayer =function (player) { 
         displayController.onCurrentPlayerSwitch(currentPlayer, player);
         currentPlayer = player;
     };
 
-    return{getCurrentPlayer, setCurrentPlayer, startGame, changedCellState};
+    const gameOver = function(isDraw, lastState){
+        const lastPlayer = getPlayerFromState(lastState);
+
+        if(isDraw){
+        displayController.showEndPanel();
+        }else{
+            displayController.showEndPanel(lastPlayer);
+        }
+
+        gameStarted = false;
+    }
+
+    const restartGame = function(){
+        location.reload();
+    }
+
+    return{getCurrentPlayer, setCurrentPlayer, startGame, changedCellState, gameOver, isGameStarted, restartGame};
 })();
 
 function clickedCell(id){
     GameBoard.trySetCellState(id);
+}
+
+function replay(){
+    gameFlow.restartGame();
 }
 
 gameFlow.startGame();
